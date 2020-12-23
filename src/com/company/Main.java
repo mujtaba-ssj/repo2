00de192dc6f292e294b410f4fdbf527f17c27199
mujtaba-ssj <naim.mujtaba@gmail.com>
2020-12-23 21:15:14 +0530

    Initial Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        int result = new Num().add(10,10);
+        System.out.println("Value is " + result);
 	
     }
 }
---
+++ src/com/company/Num.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Num {
+
+    public int add(int a,int b){
+        int sum = a+b;
+        return sum;
+    }
+}
